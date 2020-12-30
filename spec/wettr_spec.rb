d524abb0cfdf07eec5e08d7a4f35cdf58a2d3165
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-30 09:02:27 -0500

    added api key test to wettr_spec

--- spec/wettr_spec.rb
+++ spec/wettr_spec.rb
@@ -2,4 +2,8 @@ RSpec.describe Wettr do
   it "has a version number" do
     expect(Wettr::VERSION).not_to be nil
   end
+
+  it "has an api key" do
+    expect(ENV["API_KEY"]).not_to be nil
+  end
 end
